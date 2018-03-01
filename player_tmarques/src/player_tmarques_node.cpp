f14f696e58b2dc27aa65255c071b02a49e5cf4fe
TiagoSMarques <marques.tiago19@gmail.com>
2018-03-01 14:42:46 +0000

    update package

--- player_tmarques/src/player_tmarques_node.cpp
+++ player_tmarques/src/player_tmarques_node.cpp
@@ -1,10 +1,20 @@
+#include <boost/shared_ptr.hpp>
 #include <iostream>
+#include <vector>
 
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+
+#include <sstream>
+using namespace std;
+
+namespace rws_tmarques
+{
 class Player
 {
 public:
   // Constructor with the same name as the class
-  Player(std::string name)
+  Player(string name)
   {
     this->name = name;
   }
@@ -23,13 +33,13 @@ public:
         return setTeamName("blue");
         break;
       default:
-        std::cout << "wrong team index given. Cannot set team" << std::endl;
+        cout << "wrong team index given. Cannot set team" << endl;
         break;
     }
   }
 
   // Set team name, if given a correct team name (accessor)
-  int setTeamName(std::string team)
+  int setTeamName(string team)
   {
     if (team == "red" || team == "green" || team == "blue")
     {
@@ -39,43 +49,71 @@ public:
     else
     {
       this->team = "no team";
-      std::cout << "cannot set team name to " << team << std::endl;
+      cout << "cannot set team name to " << team << endl;
       return 0;
     }
   }
 
   // Gets team name (accessor)
-  std::string getTeam(void)
+  string getTeam(void)
   {
     return team;
   }
 
-  std::string name;  // A public atribute
+  string name;  // A public atribute
 
 private:
-  std::string team;
+  string team;
+};
+
+class Team
+{
+public:
+  Team(string name)
+  {
+  }
+
+  vector<string> player_names;
 };
 
 class MyPlayer : public Player
 {
 public:
-  MyPlayer(std::string argin_name, std::string argin_team) : Player(argin_name)
+  boost::shared_ptr<Team> red_team;
+  boost::shared_ptr<Team> green_team;
+  boost::shared_ptr<Team> blue_team;
+
+  MyPlayer(string argin_name, string argin_team) : Player(argin_name)
   {
+    red_team = boost::shared_ptr<Team>(new Team("red"));
+    green_team = boost::shared_ptr<Team>(new Team("green"));
+    blue_team = boost::shared_ptr<Team>(new Team("blue"));
+
     setTeamName(argin_team);
+
+    PrintReport();
+  }
+
+  void PrintReport()
+  {
+    cout << "My name is " << name << " and my team is " << getTeam() << endl;
   }
 };
+}
 
-int main()
+int main(int argc, char** argv)
 {
   // Creating an instance of class Player
-  Player player("tmarques");
-  player.setTeamName(2);
+  ros::init(argc, argv, "tmarques");
 
-  // std::cout << "somar = " << somar(.2, 2.7) << std::endl;
+  ros::NodeHandle n;
 
-  std::cout << "player.name is " << player.name << std::endl;
-  std::cout << "team is " << player.getTeam() << std::endl;
+  rws_tmarques::MyPlayer my_player("mtiago", "green");
 
-  MyPlayer my_player("mtiago", "green");
-  std::cout << "team is " << my_player.getTeam() << std::endl;
+  string test_param_value;
+  n.getParam("test_param", test_param_value);
+
+  cout << "read test_param with value " << test_param_value << endl;
+  // my_player.PrintReport();
+  ros::spin();
 }
